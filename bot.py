b818ee8a89f6c4da480d4db7590ed4d145cc9c86
petitnat521-a11y <petitnat521@gmail.com>
2026-09-05 17:38:39 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} est connecté !")
+
+@bot.command()
+async def bonjour(ctx):
+    await ctx.send(f"Bonjour {ctx.author.mention} ! 👋")
